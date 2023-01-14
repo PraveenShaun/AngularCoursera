9fafb37abed38afd041e253ef4ab409721c77a11
PraveenShaun <praveen18.warner@gmail.com>
2023-01-14 14:03:32 +0530

    asiignment2

---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Leader } from '../shared/leader';
+import { LEADERS } from '../shared/leaders'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  getLeaders(): Leader[] {
+    return LEADERS;
+  }
+
+  getFeaturedLeader(): Leader {
+    return LEADERS.find((leader) => leader.featured === true);
+  }
+
+  constructor() { }  
+}
