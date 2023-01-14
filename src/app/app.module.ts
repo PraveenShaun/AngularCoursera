66a6c3ea580639adf9250a0340d99c2e584758a0
PraveenShaun <praveen18.warner@gmail.com>
2023-01-14 12:20:24 +0530

    secondweek

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,27 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +41,10 @@ import { MenuComponent } from './menu/menu.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers:[DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
